74911549e78327b04b1da8597619403f0dcaa432
AswadArfan <156954426+AswadArfan@users.noreply.github.com>
2024-01-31 02:57:14 -0500

    Update CardTrick.java
    updated print info method

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -47,21 +47,17 @@ public class CardTrick {
     private static void printInfo() {
         System.out.println("Congratulations, you guessed right!");
         System.out.println();
-        
-        System.out.println("My name is Paul, but you can call me prof, Paul or sir");
+        System.out.println("My name is Aswad Arfan, but you can call me Aswad");
         System.out.println();
-        
         System.out.println("My career ambitions:");
         System.out.println("-- Be more active on LinkedIn");
         System.out.println("-- Have a semester with no violations of academic integrity!");
-	System.out.println();	
-
+        System.out.println();
         System.out.println("My hobbies:");
-        System.out.println("-- Investing");
-        System.out.println("-- Cooking");
-        System.out.println("-- Reading/Watching TV");
-        System.out.println("-- Riding my motorcycle");
-
+        System.out.println("-- Gaming");
+        System.out.println("-- Travelling");
+        System.out.println("-- Watching TV");
+        System.out.println("-- Car Modifications");
         System.out.println();
     }
 }
